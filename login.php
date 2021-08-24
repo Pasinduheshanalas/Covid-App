7d0cc9920f50fabc6aa38fdc8516193208d6bcc2
Pasinduheshanalas <pasindualas96@gmail.com>
2021-08-24 07:43:15 +0530

    add new file

---
+++ login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+
+    <div class="wrapper">
+
+        <section class="login">
+
+<form action="#">
+
+<header>Covid App</header>
+
+<div class="error-text">This is an error massage</div>
+
+<div class="feilds input">
+    <label>Email Adress</label>
+    <input type="text" placeholder="Enter Your Email ">
+
+</div>
+
+<div class="feilds input">
+    <label>password</label>
+    <input type="text" placeholder="Enter password">
+   <i class="fa fa-eye"></i>
+
+</div>
+
+
+
+<div class="feilds button ">
+    
+    <input type="submit" value="Login">
+
+</div>
+
+
+
+
+<div class="link">Not yet signup <a href="#">Signup Now</a></div>
+
+
+
+
+
+
+</form>
+
+
+        </section>
+
+
+</div>
+
+
+    
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <link rel="stylesheet" href="signup.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="javascript/signup.js"></script>
+   
+
+</head>
+<body>
+
+
+  
+
+    <div class="wrapper">
+
+        <section class="form signup">
+    
+    
+           
+    
+            <form action="#">
+    
+                <header>Covid App</header>
+    
+                <div class="error-text">This is an error massage</div>
+    
+    
+    
+                <div class="name-details">
+    
+                    <div class="feilds input">
+                        <label>First name</label>
+                        <input type="text" placeholder="First Name">
+    
+                    </div>
+    
+                    <div class="feilds input">
+                        <label>Last name</label>
+                        <input type="text" placeholder="Last Name">
+                       
+    
+                    </div>
+                </div>
+    
+    
+    
+                    <div class="feilds input">
+                        <label>Email Adress</label>
+                        <input type="text" placeholder="Enter Your Email ">
+    
+                    </div>
+    
+                    <div class="feilds input">
+                        <label>password</label>
+                        <input type="text" placeholder="Enter password">
+                       
+    
+                    </div>
+    
+                    <div class="feilds Image">
+                        <label>Select Image</label>
+                        <input type="file">
+    
+                    </div>
+    
+                    
+                    <div class="feilds button ">
+                        
+                        <input type="submit" value="Signup">
+    
+                    </div>
+    
+    
+    
+    
+                    <div class="link">alredy signup <a href="#">Login Now</a></div>
+    
+    
+            </form>
+    
+            
+    
+        </section>
+    </div>
+
+</body>
+</html>
